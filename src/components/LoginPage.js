42f086a2e6fc510bca6c05f2e0861386739ccd89
toufiq23 <drrameraaron023@gmail.com>
2020-05-14 11:48:45 +0200

    Add styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,17 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+		<img id="tcup" src='/images/t-cup.jpeg'/>
+      <h1 className="box-layout__title">T-Budgets App</h1>
+      <p className="intro">This is a budgeting app. In this app you can record your expenses in our database and then access it by logging in and logging out. Thanks for joining our vast database.</p>
+      <button className="button" onClick={startLogin}>Login with Google</button>
+    </div>
+		<div>
+			<h2>Made by M Toufiq ELAHI</h2>
+			<img className="portrait" src="/images/me.jpg" />
+		</div>
   </div>
 );
 
